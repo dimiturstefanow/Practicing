867f35c57a469fb475de93d5ff29fe909981ee9c
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-15 12:06:02 +0300

    mastered palindrome

--- String Manipulation/checkForPalindrome03.js
+++ String Manipulation/checkForPalindrome03.js
@@ -1,12 +1,12 @@
-// check palidrom easiest way
+// simpliest way to check palindrome
 
-const str = "Racecar!"
+const str = "Racecar!";
 
-function isPalindrome(str){
-    str = str.replace(/\W/g, "")
-    str = str.toLowerCase();
+function isPalindrome(str) {
+  str = str.replace(/\W/g, "");
+  str = str.toLowerCase();
 
-    return str === str.split("").reverse().join("")
+  return str === str.split("").reverse().join("");
 }
 
 console.log(isPalindrome(str));
