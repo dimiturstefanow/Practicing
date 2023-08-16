bdc533e100d949f3e06e58c84c0bccc2cd91717a
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-16 21:02:01 +0300

    validating my knowledge from the day before, I still know how to do palidrome

--- String Manipulation/checkForPalindrome03.js
+++ String Manipulation/checkForPalindrome03.js
@@ -1,12 +1,14 @@
-// Simpliest way
+// easiest way to check palindrome
 
-const str = "Anna!";
+const str = "racecar";
 
 function isPalindrome(str) {
   str = str.replace(/\W/g, "");
+
   str = str.toLowerCase();
 
   return str === str.split("").reverse().join("")
 }
+
 
 console.log(isPalindrome(str));
