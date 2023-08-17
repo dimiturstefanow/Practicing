c04704c8d73066f4c7f56b0a78bd3980c1bd61e6
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-17 08:34:22 +0300

    palindrome check

--- String Manipulation/anagram.js
+++ String Manipulation/anagram.js
@@ -1,21 +1,10 @@
-// Easy way to find anagram
+// Easy way to find whether string is anagram or not
 
-function isAnagram(str1, str2) {
- sorted1 = str1.toLowerCase().split("").sort().join("");
- sorted2 = str2.toLowerCase().split("").sort().join("");
-
-  return (sorted1 === sorted2);
-}
-
-console.log(isAnagram("qsha", "qsHa"));
-
-/*
 function isAnagram(str1, str2){
-    sorted1 = str1.split("").sort().join("").toLowerCase();
-    sorted2 = str2.split("").sort().join("").toLowerCase();
+    sorted1 = str1.toLowerCase().split("").sort().join("")
+    sorted2 = str2.toLowerCase().split("").sort().join("")
 
     return sorted1 === sorted2
 }
 
-console.log(isAnagram("qsha", "qsHa")); // here I was receiving problem, this was false, I was expecting true, right above is the fixed version(uncommented)
-*/
+console.log(isAnagram("Bratchetomi", "chetomibrat")); // true
--- String Manipulation/checkForPalindrome03.js
+++ String Manipulation/checkForPalindrome03.js
@@ -1,14 +1,14 @@
 // easiest way to check palindrome
 
-const str = "racecar";
+const str = "racecar" // true
 
-function isPalindrome(str) {
+function isPalindrome(str){
   str = str.replace(/\W/g, "");
-
   str = str.toLowerCase();
 
   return str === str.split("").reverse().join("")
-}
 
+
+}
 
 console.log(isPalindrome(str));
