18aeaaa013ab72c4b216b4d84934089926356544
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-17 06:43:59 +0300

    synch

--- String Manipulation/anagram.js
+++ String Manipulation/anagram.js
@@ -1,5 +1,15 @@
-// Easiest way to find is it anagram
+// Easy way to find anagram
 
+function isAnagram(str1, str2) {
+ sorted1 = str1.toLowerCase().split("").sort().join("");
+ sorted2 = str2.toLowerCase().split("").sort().join("");
+
+  return (sorted1 === sorted2);
+}
+
+console.log(isAnagram("qsha", "qsHa"));
+
+/*
 function isAnagram(str1, str2){
     sorted1 = str1.split("").sort().join("").toLowerCase();
     sorted2 = str2.split("").sort().join("").toLowerCase();
@@ -7,5 +17,5 @@ function isAnagram(str1, str2){
     return sorted1 === sorted2
 }
 
-console.log(isAnagram("Hello", "lHelo"));
-
+console.log(isAnagram("qsha", "qsHa")); // here I was receiving problem, this was false, I was expecting true, right above is the fixed version(uncommented)
+*/
