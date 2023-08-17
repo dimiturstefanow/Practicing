60965d6aa5b805d3ab18ccedc624a2c1b5203fa4
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-17 05:45:55 +0300

    anagram

---
+++ String Manipulation/anagram.js
@@ -0,0 +1,11 @@
+// Simpliest way to find wheter it is anagram or not
+
+function isAnagram(str1, str2) {
+  let sorted1 = str1.split("").sort().join("").toLowerCase();
+  let sorted2 = str2.split("").sort().join("").toLowerCase();
+
+  return sorted1 === sorted2;
+}
+
+console.log(isAnagram("hello", "jello"));
+console.log(isAnagram("hello", "loelh"));
