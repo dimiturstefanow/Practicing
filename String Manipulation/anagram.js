3680e96db8afb84475c5c9eddd7530020c5ce2ed
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-18 09:57:17 +0300

    verifying

--- String Manipulation/anagram.js
+++ String Manipulation/anagram.js
@@ -1,10 +1,11 @@
-// Easy way to find whether string is anagram or not
+// Simpliest way to find if string is anagram
 
-function isAnagram(str1, str2){
- sorted1 = str1.toLowerCase().split("").sort().join("")
- sorted2 = str2.toLowerCase().split("").sort().join("")
+function isAnagram(str1, str2) {
+sorted1 = str1.toLowerCase().split("").sort().join("");
+sorted2 = str2.toLowerCase().split("").sort().join("");
 
- return sorted1 === sorted2
+return sorted1 === sorted2
+
 }
 
-console.log(isAnagram("Hello", "hleelo"));
+console.log(isAnagram("dabebrat", "bratdabe"));
