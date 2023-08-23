e89be2f84734d7963c593e7c0f1e111ea32f8879
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-23 08:55:11 +0300

    checking if knowledge retained

--- String Manipulation/anagram.js
+++ String Manipulation/anagram.js
@@ -1,11 +1,14 @@
 // Simpliest way to find if string is anagram
 
+str1 = "racercar";
+str2 = "racercar";
+
 function isAnagram(str1, str2) {
-sorted1 = str1.toLowerCase().split("").sort().join("");
-sorted2 = str2.toLowerCase().split("").sort().join("");
+  sorted1 = str1.toLowerCase().split("").reverse().join("");
+  sorted2 = str2.toLowerCase().split("").reverse().join("");
 
-return sorted1 === sorted2
-
+  return sorted1 === sorted2
 }
 
-console.log(isAnagram("dabebrat", "bratdabe"));
+
+console.log(isAnagram(str1, str2));
