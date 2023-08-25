641441ef04b728fef8bed5d10147e2e49125b35f
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-25 17:48:22 +0300

    knowledge about anagram

--- String Manipulation/anagram.js
+++ String Manipulation/anagram.js
@@ -1,14 +1,15 @@
 // Simpliest way to find if string is anagram
 
-str1 = "racercar";
-str2 = "racercar";
+const str1 = "civic"
+const str2 = "civic"
 
-function isAnagram(str1, str2) {
-  sorted1 = str1.toLowerCase().split("").reverse().join("");
-  sorted2 = str2.toLowerCase().split("").reverse().join("");
+function isAnagram(str1, str2){
+sorted1 = str1.toLowerCase().split("").reverse().join("");
+sorted2 = str2.toLowerCase().split("").reverse().join("");
 
-  return sorted1 === sorted2
+return sorted1 === sorted2
+
+
 }
-
 
 console.log(isAnagram(str1, str2));
